b422821a48150c39895b4c9542faf15db3175d14
Tim Chagnon <tachagnon@gmail.com>
2016-01-14 23:34:06 -0800

    Open file and count lines

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,21 @@
-print("Hello, World!")
+import Foundation
+
+func main(args: [String]) {
+  if args.count < 2 {
+    print("Usage: raylang-swift <model.smf>")
+    return
+  }
+
+  print("cwd: " + NSFileManager.defaultManager().currentDirectoryPath)
+
+  let path = args[1]
+  guard let smf = try? String(contentsOfFile: path, encoding: NSUTF8StringEncoding) else {
+    print("Unable to read file: \(path)")
+    return
+  }
+
+  let lines = smf.componentsSeparatedByCharactersInSet(NSCharacterSet.newlineCharacterSet())
+  print("lines in \(path): \(lines.count)")
+}
+
+main(Process.arguments)
